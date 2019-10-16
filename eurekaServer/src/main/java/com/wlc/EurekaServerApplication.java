4a986495eb374e2044fda2cd467d549a2edb937f
Administrator <wanglichaoya@163.com>
2019-10-16 14:30:02 +0800

    优化 检测端口号是否被占用，放到 公共项目中的工具类中，直接调用工具类的方法就可以了。

--- eurekaServer/src/main/java/com/wlc/EurekaServerApplication.java
+++ eurekaServer/src/main/java/com/wlc/EurekaServerApplication.java
@@ -1,6 +1,6 @@
 package com.wlc;
 
-import cn.hutool.core.util.NetUtil;
+import com.wlc.util.CheckPortAbledUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -15,13 +15,17 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
 
+    /*@Autowired
+    CheckPortAbledUtil checkPortAbledUtil;*/
     public static void main(String[] args) {
         //8761 这个端口是默认的，就不要修改了，后面的子项目，都会访问这个端口。
         int port = 8761;
-        if(!NetUtil.isUsableLocalPort(port)) {
+        /*if(!NetUtil.isUsableLocalPort(port)) {
             System.err.printf("端口%d被占用了，无法启动%n", port );
             System.exit(1);
-        }
+        }*/
+        //测试端口是否被占用了
+        CheckPortAbledUtil.checkPortAbled(port);
         new SpringApplicationBuilder(EurekaServerApplication.class).properties("server.port=" + port).run(args);
     }
 }
