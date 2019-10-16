5f3c0cf1c633604a7ab21d709e36e8065b18ef88
Administrator <wanglichaoya@163.com>
2019-10-16 14:33:11 +0800

    优化 检测端口号是否被占用，放到 公共项目中的工具类中，直接调用工具类的方法就可以了； 优化5秒内输入端口号，否则就用默认的端口号。目的是：模拟集群

--- product-data-service2/src/main/java/com/wlc/ProductDataServiceApplication.java
+++ product-data-service2/src/main/java/com/wlc/ProductDataServiceApplication.java
@@ -1,18 +1,10 @@
 package com.wlc;
 
-import cn.hutool.core.convert.Convert;
-import cn.hutool.core.thread.ThreadUtil;
-import cn.hutool.core.util.NetUtil;
-import cn.hutool.core.util.NumberUtil;
+import com.wlc.util.CheckPortAbledUtil;
+import com.wlc.util.FutureTimeOutSecondsUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * describe:启动类， 考虑到要做集群。 所以让用户自己输入端口，推荐 8001，8002，8003.
@@ -33,7 +25,7 @@ public class ProductDataServiceApplication {
     public static void main(String[] args){
       int port = 0;
       int defaultPort = 8001;
-        Future<Integer> future = ThreadUtil.execAsync(() ->{
+       /* Future<Integer> future = ThreadUtil.execAsync(() ->{
           int p = 0;
             System.out.println("请于5秒钟内输入端口号, 推荐  8001 、 8002  或者  8003，超过5秒将默认使用 " + defaultPort);
             Scanner scanner = new Scanner(System.in);
@@ -50,16 +42,20 @@ public class ProductDataServiceApplication {
                }
             }
             return p;
-        });
-        try{
+        });*/
+       /* try{
             port=future.get(5, TimeUnit.SECONDS);
         }catch (InterruptedException | ExecutionException |TimeoutException e) {
             port = defaultPort;
-        }
-        if(!NetUtil.isUsableLocalPort(port)){
+        }*/
+       /* if(!NetUtil.isUsableLocalPort(port)){
             System.err.printf("端口%d被占用了，无法启动%n",port);
             System.exit(1);
-        }
+        }*/
+        //让 5秒内 输入端口号，如果超过5秒的时候，就用默认的端口号
+        port = FutureTimeOutSecondsUtil.futureTimeOutSecond(5,defaultPort);
+        //测试端口号是否被占用
+        CheckPortAbledUtil.checkPortAbled(port);
         new SpringApplicationBuilder(ProductDataServiceApplication.class).properties("server.port=" + port).run(args);
     }
 }
