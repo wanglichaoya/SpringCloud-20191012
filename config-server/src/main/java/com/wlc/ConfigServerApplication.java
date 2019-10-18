6dbeb52a308d1c8f71c48ee68bc7e9898e440312
Administrator <wanglichaoya@163.com>
2019-10-18 11:56:32 +0800

    新增了一些注解 含义的解释

--- config-server/src/main/java/com/wlc/ConfigServerApplication.java
+++ config-server/src/main/java/com/wlc/ConfigServerApplication.java
@@ -9,11 +9,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import cn.hutool.core.util.NetUtil;
 
 @SpringBootApplication
+//@EnableConfigServer:注解表示这个SpringBoot 是一个配置服务器
 @EnableConfigServer
 @EnableDiscoveryClient
 @EnableEurekaClient
 public class ConfigServerApplication {
     public static void main(String[] args) {
+        //使用的端口号是8030
         int port = 8030;
         if (!NetUtil.isUsableLocalPort(port)) {
             System.err.printf("端口%d被占用了，无法启动%n", port);
