d100b98b30e987a3f9a230b96c8ffbd4df5c7cf5
Administrator <wanglichaoya@163.com>
2019-10-18 12:01:26 +0800

    新增： SpringCloud 配置客户端， 这样例如一些 版本的信息，可以在git上面更改，然后客户端的版本信息也会相应更改。 这样的好处是：    随着以后 试图为服务的增加，需要在很多的视图微服务中维护版本信息，这样直接在git上面维护就可以，很方便

--- product-view-service-feign/src/main/java/com/wlc/controller/ProductController.java
+++ product-view-service-feign/src/main/java/com/wlc/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.wlc.controller;
 import com.wlc.pojo.Product;
 import com.wlc.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,16 +52,19 @@ import java.util.List;
  * @date 2019/10/16
  */
 @Controller
+@RefreshScope //配置文件自动刷新
 public class ProductController {
+    @Value("${version}")
+    String version;
 
     @Autowired
     ProductService productService;
 
     @RequestMapping("/products")
     public Object products(Model model){
-        System.out.println("到达 feign");
         List<Product> productList = productService.listProducts();
         model.addAttribute("ps",productList);
+        model.addAttribute("version",version);
         return "products";
     }
 }
